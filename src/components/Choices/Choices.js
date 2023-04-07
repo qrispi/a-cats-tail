c493fa59a9eb3c5eef91f9091d02422144b9efce
Jeff Sahim <jeffreysahim@gmail.com>
2023-04-07 17:45:09 -0400

    Add new Choices, Day and Result components with corresponding skeletons

---
+++ src/components/Choices/Choices.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";  
+import '../Choices/Choices.css'
+
+function Choices(){
+
+  return (
+    <>
+      <h3>What do you do?</h3>
+      <button>Good Option Text</button>
+      <button>Neutral Option Text</button>
+      <button>Evil Option Text</button>
+      <button>Consult The Cat Bible</button>
+    </>
+  )
+}
+
+export default Choices;

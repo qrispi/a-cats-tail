24f5b15265217056077565fec89e907e4aa22fab
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-08 14:52:00 -0600

    Add fetch call to Book component

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './Book.css';
+import React, { useEffect } from "react";
+import "./Book.css";
 
 function Book() {
-    
+  const getFact = async () => {
+    try {
+      const response = await fetch("https://meowfacts.herokuapp.com/");
+      const catFact = await response.json();
+      console.log(catFact);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getFact()
+  }, []);
+
+  return (
+    <>
+      <h1>this is a cat fact</h1>
+    </>
+  );
 }
 
 export default Book;

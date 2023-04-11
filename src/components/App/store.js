e19406a8a03c4004f9109ab6ddce7aa7828126b1
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-11 10:44:50 -0600

    Add catNameReducer to store

--- src/components/App/store.js
+++ src/components/App/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dayReducer from "../../features/daySlice";
+import catNameReducer from "../../features/nameSlice"
 
 export const store = configureStore({
   reducer: {
     day: dayReducer,
+    catName: catNameReducer
   },
 });
 

50000d0c4a095083247ccd34529fbe55f5253e29
Christopher Baum <christopherabaum@gmail.com>
2023-04-12 12:26:19 -0500

    Add morality and reducers for it to catSlice

--- src/features/catSlice.js
+++ src/features/catSlice.js
@@ -2,13 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const catSlice = createSlice({
     name: "cat",
-    initialState: { name: ''},
+    initialState: { name: '', morality: 0},
     reducers: {
         updateCatName: (state, action) => {
-            state.name = action.payload
+            state.name = action.payload;
+        },
+        incrementCatMorality: (state) => {
+            state.morality += 1;
+        },
+        decrementCatMorality: (state) => {
+            state.morality -= 1;
         }
     }
 });
 
-export const { updateCatName } = catSlice.actions;
-export default catSlice.reducer
+export const { updateCatName, incrementCatMorality, decrementCatMorality } = catSlice.actions;
+export default catSlice.reducer;

23d116f82513c0578d62bd7a4e7244d290b6c659
Jesus Borjas <jesusbor006@gmail.com>
2023-04-13 00:02:38 -0500

    Displays different content at the end depending on cats morality

--- src/components/Finale/FinaleType.js
+++ src/components/Finale/FinaleType.js
@@ -1,9 +1,25 @@
 import "./FinaleType.css";
+import { useSelector } from "react-redux";
 
 function FinaleType() {
+  const catMorality = useSelector((state) => state.cat.morality);
+  const getFinaleType = () => {
+    let finaleType;
+    if (catMorality > 3) {
+      finaleType =
+        "Fluffly wins the nobel peace prize for solving global warming";
+    } else if (catMorality <= 3 && catMorality >= 0) {
+      finaleType = `Fluffy says "This isn't really working out anymore" and packs a bag. All you know is they "want to do something with Turquoise"`;
+    } else {
+      finaleType =
+        "Fluffy becomes El Gato the most feared drug lord in the world";
+    }
+    return finaleType;
+  };
+
   return (
     <div className="finale-type-container">
-      <h1>Good Cat!</h1>
+      <p>{getFinaleType()}</p>
       <button>Play Again</button>
       <button>Read the Book</button>
     </div>

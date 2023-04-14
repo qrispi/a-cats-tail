5f5e782caeb4774b0ed43a661025b17dc2d060ce
Christopher Baum <christopherabaum@gmail.com>
2023-04-13 22:02:36 -0500

    Add conditional rendering and error handling to removing bookmarks inside Page

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Page.css";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { addFact, removeFact } from "../../features/bookSlice";
+import { removeFact } from "../../features/bookSlice";
 
 function Page() {
     const savedFacts = useSelector((state) => state.book.facts);
@@ -17,12 +17,12 @@ function Page() {
     <>
       <h2>Page {pageNum + 1}</h2>
       <p>{savedFacts[pageNum]}</p>
+      {!savedFacts[pageNum] && <p>Oh no! You ripped out this page! Use the buttons to go to a different page.</p>}
       <NavLink to="/book">
         <button>BACK</button>
       </NavLink>
       {pageNum > 0 && <button onClick={() => turnPage(-1)}>Prev Page</button>}
-      {/* {!savedFacts.includes(fact) && <button className="save-fact" onClick={() => dispatch(addFact(fact))}>Bookmark Fact</button>} */}
-      {/* {savedFacts.includes(fact) && <button onClick={() => dispatch(removeFact(fact))}>Remove Bookmark</button>} */}
+      {savedFacts.includes(savedFacts[pageNum]) && <button onClick={() => dispatch(removeFact(savedFacts[pageNum]))}>Remove Bookmark</button>}
       {savedFacts.length >= (pageNum + 2) && <button onClick={() => turnPage(1)}>Next Page</button>}
     </>
   );

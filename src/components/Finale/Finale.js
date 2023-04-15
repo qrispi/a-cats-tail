0a298fbaf7888618d667340494b863c44e2919a2
Jeff Sahim <jeffreysahim@gmail.com>
2023-04-15 10:47:53 -0400

    Refactor catImage display functionality

--- src/components/Finale/Finale.js
+++ src/components/Finale/Finale.js
@@ -1,32 +1,28 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import "./Finale.css";
 
 function Finale() {
   const catMorality = useSelector((state) => state.cat.morality);
-
-  const getCatType = () => {
-    if (catMorality > 0) {
-      return '../../images/good-cat-fly.gif';
-    } else if (catMorality === 0) {
-      return '../../images/neutral-cat.gif';
-    } else {
-      return '../../images/evil-cat.gif';
-    }
-  }
+  const [catPath, setCatPath] = useState()
+  
 
   const getFinaleType = () => {
     if (catMorality > 0) {
+      setCatPath('../../images/good-cat-fly.gif');
       return "Fluffly wins the nobel peace prize for solving global warming";
     } else if (catMorality === 0) {
+      setCatPath('../../images/neutral-cat.gif')
       return `Fluffy says "This isn't really working out anymore" and packs a bag. All you know is they "want to do something with Turquoise"`;
     } else {
+      setCatPath('../../images/evil-cat.gif')
       return "Fluffy becomes El Gato the most feared drug lord in the world";
     }
   };
 
   return (
     <div className="finale-type-container">
-      <img src={require(getCatType())} />
+      <img src={require(catPath)} />
       <p>{getFinaleType()}</p>
       <button>Play Again</button>
       <button>Read the Book</button>

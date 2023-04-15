e67d77980a5fc21705eb1e543f31f6cc64d5c906
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-14 18:55:55 -0600

    Add tests to book.cy.js

--- cypress/e2e/book.cy.js
+++ cypress/e2e/book.cy.js
@@ -9,12 +9,23 @@ describe("Cat Fact Book", () => {
   });
 
 
-  it('As a user, I should be able open the book by clicking the "Consult The Cat Fact Book" button', () => {
-    
+  it('should be able open the book by clicking the "Consult The Cat Fact Book" button', () => {
+    cy.get('button').should('have.text', 'Continue').click()
+      .get('button').should('have.text', 'CONTINUE').click()
+      .get('button').eq(3).should('have.text', 'Consult The Cat Fact Book').click()
+    cy.contains('h1', 'The Book of Cat')
   });
 
-  it('As a user, when I click the Cat Fact Book, I should be taken to a new page that shows me a random Cat Fact', () => {
-    // click button, intercept API, 
+  it('when the Cat Fact Book is clicked, user should be taken to a new page that shows me a random Cat Fact', () => {
+    cy.get('button').should('have.text', 'Continue').click()
+      .get('button').should('have.text', 'CONTINUE').click()
+      .get('button').eq(3).should('have.text', 'Consult The Cat Fact Book').click()
+    cy.contains('h1', 'The Book of Cat')
+    cy.contains('People who are allergic to cats are actually allergic to cat saliva or to cat dander. If the resident cat is bathed regularly the allergic people tolerate it better.')
+  })
+
+  it('Bookmarking a fact should save it in My Bookmarks', () => {
+
   })
 
 

98ece5e920918dc8d5132dd27e4857c31727ec73
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-04-15 11:07:55 -0600

    Add tests to book.cy.js

--- cypress/e2e/book.cy.js
+++ cypress/e2e/book.cy.js
@@ -7,7 +7,6 @@ describe('Cat Fact Book', () => {
     });
   });
 
-
   it('should be able open the book by clicking the "Consult The Cat Fact Book" button', () => {
     cy.get('button').should('have.text', 'Continue').click()
       .get('button').should('have.text', 'CONTINUE').click()
@@ -27,10 +26,26 @@ describe('Cat Fact Book', () => {
     cy.get('button').should('have.text', 'Continue').click()
     .get('button').should('have.text', 'CONTINUE').click()
     .get('button').eq(3).should('have.text', 'Consult The Cat Fact Book').click()
-    
+    .get('button').eq(2).should('have.text', 'Bookmark Fact').click()
+    .get('button').eq(2).should('have.text', 'Remove Bookmark')
   })
 
   it('Clicking the Remove Bookmark button should remove that fact from My Bookmarks', () => {
+    cy.get('button').should('have.text', 'Continue').click()
+    .get('button').should('have.text', 'CONTINUE').click()
+    .get('button').eq(3).should('have.text', 'Consult The Cat Fact Book').click()
+    .get('button').eq(2).should('have.text', 'Bookmark Fact').click()
+    .get('button').eq(2).should('have.text', 'Remove Bookmark').click()
+    .get('button').eq(2).should('have.text', 'Bookmark Fact')
+  })
+
+  it('when "Get a new fact" button is clicked, should display another random fact', () => {
+    cy.get('button').should('have.text', 'Continue').click()
+      .get('button').should('have.text', 'CONTINUE').click()
+      .get('button').eq(3).should('have.text', 'Consult The Cat Fact Book').click()
+    cy.contains('People who are allergic to cats are actually allergic to cat saliva or to cat dander. If the resident cat is bathed regularly the allergic people tolerate it better.')
+      .get('button').eq(1).should('have.text', 'Get a new fact!').click()
+ 
 
   })
 
